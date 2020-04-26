f6b86b97edf8c4c8a4eceff578cdf7e7365cb86a
SamuelDarras <slamicote@gmail.com>
2020-04-26 17:52:04 +0200

    Ajout classe abstraite Ghost.java

---
+++ src/Entity/Ghost.java
@@ -0,0 +1,12 @@
+package Entity;
+
+public abstract class Ghost extends Personnage{
+    //public Ghost(double x, double y, double speed);
+    public Ghost(double x, double y, double baseSpeed) {
+        super(x, y, baseSpeed);
+    }
+
+    public abstract void tick();
+
+    public abstract void AI();
+}
