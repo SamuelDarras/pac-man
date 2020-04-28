c5e42048b783d0fca383969b03a910695c7d4bd2
SamuelDarras <slamicote@gmail.com>
2020-04-28 18:03:59 +0200

    suppr.commentaire

--- src/Entity/Ghost.java
+++ src/Entity/Ghost.java
@@ -1,7 +1,6 @@
 package Entity;
 
 public abstract class Ghost extends Personnage{
-    //public Ghost(double x, double y, double speed);
     public Ghost(double x, double y, double baseSpeed) {
         super(x, y, baseSpeed);
     }
