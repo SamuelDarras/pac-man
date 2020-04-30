42f85fefa32ec2a69ac82e7d0d8d705603e22659
SamuelDarras <slamicote@gmail.com>
2020-04-30 11:24:54 +0200

    Ajout Items

---
+++ src/Entity/Items.java
@@ -0,0 +1,14 @@
+package Entity;
+
+public class Items {
+
+    private int score;
+
+    public Items(int score){
+        this.score = score;
+    }
+
+    public int getScore() {
+        return score;
+    }
+}
