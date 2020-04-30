8618646beaa36bed94b5bc76a34e6b66e2d9b0dc
SamuelDarras <slamicote@gmail.com>
2020-04-30 11:24:42 +0200

    Ajout PacGomme

---
+++ src/Entity/PacGomme.java
@@ -0,0 +1,15 @@
+package Entity;
+
+public class PacGomme extends Items{
+
+    private boolean afficheScore = false;
+
+
+    public PacGomme(){
+        super(10);
+    }
+
+    public boolean isAfficheScore() {
+        return afficheScore;
+    }
+}
