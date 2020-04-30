e89dd8ff0b73360d51aa83dc486207bb4bf6cefe
SamuelDarras <slamicote@gmail.com>
2020-04-30 11:24:21 +0200

    Ajout SuperPacGomme

---
+++ src/Entity/SuperPacGomme.java
@@ -0,0 +1,14 @@
+package Entity;
+
+public class SuperPacGomme extends Items{
+
+    private boolean afficheScore = true;
+
+    public SuperPacGomme(){
+       super(100);
+    }
+
+    public boolean isAfficheScore() {
+        return afficheScore;
+    }
+}
