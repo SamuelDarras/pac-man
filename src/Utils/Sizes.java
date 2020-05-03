16d401b793fd54c3c6573557affec5f333eab5df
SamuelDarras <slamicote@gmail.com>
2020-05-03 21:43:33 +0200

    Ajout Sizes pour centraliser les tailles de tous les éléments

---
+++ src/Utils/Sizes.java
@@ -0,0 +1,8 @@
+package Utils;
+
+public class Sizes {
+    public static int PERSONNAGE_SIZE   = 2;
+    public static int FRUIT_SIZE        = 2;
+    public static int ITEM_DEFAULT_SIZE = 1;
+    public static int WALL_SIZE         = 1;
+}
