951af9c19a7df95d04e533f881f2a7f85b4a4075
SamuelDarras <slamicote@gmail.com>
2020-05-03 14:27:04 +0200

    Fix parceque voila

--- src/Game/Plateau.java
+++ src/Game/Plateau.java
@@ -1,42 +1,52 @@
+package Game;
+import Entity.*;
+
 import java.io.*;
 
-
 public class Plateau{
-    int[][] plateau;
+    Entity[] plateau;
 
     public Plateau(String levelPath) throws Exception{
-        creerPlateau(levelPath);
+        remplirPlateau(levelPath);
     }
 
-    public int compteLigne(String levelPath)throws Exception{
+    public void remplirPlateau(String levelPath)throws Exception{
         BufferedReader read = new BufferedReader(new FileReader(levelPath));
-        int compteur = 0 ;
-        while (read.readLine()!=null)
-            compteur++;
-        read.close();
-        return compteur;
-    }
-    public int compteColone(String levelPath)throws Exception{
-        BufferedReader read = new BufferedReader(new FileReader(levelPath));
-        int cC = read.readLine().length();
-        read.close();
-        return cC;
-    }
-
-    public void creerPlateau(String levelPath)throws Exception{
-        plateau = new int[compteLigne(levelPath)][compteColone(levelPath)];
-        BufferedReader read = new BufferedReader(new FileReader(levelPath));
-        String t;
-        for(int i = 0;i<plateau.length;i++){
+        String t = read.readLine();
+        String[] lst = t.split(" ");
+        int[][] murs = new int[Integer.parseInt(lst[0])][Integer.parseInt(lst[1])];
+        Items[][] items = new Items[Integer.parseInt(lst[0])][Integer.parseInt(lst[1])];
+        int larg = murs[0].length;
+        for(int i = 0;i<murs.length;i++){
             t=read.readLine();
-            for (int j = 0 ; j<plateau[0].length;j++){
-                plateau[i][j]=t.charAt(j);
+            for (int j = 0 ; j<larg;j++){
+                if (t.charAt(j)=='1') {
+                    plateau[larg*i+j] = new Wall(j,i);
+                }
+                if (t.charAt(j)!='0'){
+                    if (t.charAt(j)=='p')
+                        plateau[larg*i+j] = new PacGomme();
+                    else
+                        plateau[larg*i+j] = new SuperPacGomme();
+                }else {
+                    plateau[larg*i+j] = null;
+                }
+
             }
         }
+        read.close();
+    }
+    public Entity getIndex(int index){
+        return plateau[index];
     }
 
-    //public static void main(String[] args)throws Exception {
-    //    Plateau j = new Plateau("level1.txt");
-    //}
+    public static void main(String[] args)throws Exception {
+        Plateau j = new Plateau("level1.txt");
+        for(int i=0; i<j.plateau.length; i++)
+            if (i%28==0){
+                System.out.println();
+                System.out.print(j.getIndex(i));
+            }
+    }
 
 }
