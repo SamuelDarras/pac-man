0bc389edbbf652fabcdbfc762b39f3a0c49e3662
SamuelDarras <slamicote@gmail.com>
2020-05-03 21:46:17 +0200

    Modif. en accord avec l'ajout de Sizes

--- src/Entity/Items.java
+++ src/Entity/Items.java
@@ -1,11 +1,13 @@
 package Entity;
 
+import Utils.Sizes;
+
 public class Items extends Entity{
 
     private int score;
 
     public Items(int score, double x, double y){
-        super(x, y, 1, 1);
+        super(x, y, Sizes.ITEM_DEFAULT_SIZE, Sizes.ITEM_DEFAULT_SIZE);
         this.score = score;
     }
 
