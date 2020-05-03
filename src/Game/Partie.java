bfc16d5d9f986a73cd37f91c7ac057e00d33dd86
SamuelDarras <slamicote@gmail.com>
2020-05-03 21:19:23 +0200

    Ajout de plein de trucs dans Partie

--- src/Game/Partie.java
+++ src/Game/Partie.java
@@ -1,7 +1,16 @@
 package Game;
 
+import Entity.Fruit;
+
 public class Partie {
     private Score score;
+
+
+    private boolean action = true;
+    boolean quitter = false;
+    long total = 0;
+
+
     public Partie() {
         score = new Score();
     }
@@ -9,4 +18,46 @@ public class Partie {
     public void scoreAdd(int s) {
         score.scoreAdd(s);
     }
+
+    public void run() throws Exception {
+        Plateau p1 = new Plateau("/home/samuel/pacman/src/levels/level1V2.txt");
+
+
+        int sec = 0;
+
+        while (!quitter) {
+            long start = System.currentTimeMillis();
+            //System.out.println(total);
+            long finish = System.currentTimeMillis();
+            total += finish - start;
+
+
+            if (sec != total / 1000) {
+                sec = (int) (total / 1000);
+                System.out.println(sec);
+                action = true;
+            }
+
+            doSomthingAt(1000, () -> {
+                if ( !(p1.getIndex(p1.getIdxFruit()) instanceof Fruit) ) {
+                    System.out.println("OOF !!");
+                    p1.setIndex(p1.getIdxFruit(), new Fruit(200, 10, 10));
+                }
+            });
+
+            doSomthingAt(3000, () -> quitter = true);
+
+        }
+    }
+
+    private void doSomthingAt(long timing, Callback callback) {
+        if (action && total == timing) {
+            callback.call();
+            action = false;
+        }
+    }
+}
+
+interface Callback {
+    void call();
 }
