a2f44bd483e231520c4971f45f24a1f332221fea
SamuelDarras <slamicote@gmail.com>
2020-05-03 15:37:06 +0200

    Je suis toujours pas sûr ...

--- src/Game/Plateau.java
+++ src/Game/Plateau.java
@@ -1,37 +1,38 @@
 package Game;
+
 import Entity.*;
 
 import java.io.*;
 
-public class Plateau{
+public class Plateau {
 
 
     Entity[] plateau;
 
-    public Plateau(String levelPath) throws Exception{
+    public Plateau(String levelPath) throws Exception {
         remplirPlateau(levelPath);
     }
 
-    public void remplirPlateau(String levelPath)throws Exception{
+    public void remplirPlateau(String levelPath) throws Exception {
         BufferedReader read = new BufferedReader(new FileReader(levelPath));
         String t = read.readLine();
         String[] lst = t.split(" ");
         int[][] murs = new int[Integer.parseInt(lst[0])][Integer.parseInt(lst[1])];
         Items[][] items = new Items[Integer.parseInt(lst[0])][Integer.parseInt(lst[1])];
         int larg = murs[0].length;
-        for(int i = 0;i<murs.length;i++) {
-            t=read.readLine();
-            for (int j = 0 ; j<larg;j++){
-                if (t.charAt(j)=='1') {
-                    plateau[larg*i+j] = new Wall(j,i);
+        for (int i = 0; i < murs.length; i++) {
+            t = read.readLine();
+            for (int j = 0; j < larg; j++) {
+                if (t.charAt(j) == '1') {
+                    plateau[larg * i + j] = new Wall(i, j);
                 }
-                if (t.charAt(j)!='0'){
-                    if (t.charAt(j)=='p')
-                        plateau[larg*i+j] = new PacGomme();
+                if (t.charAt(j) != '0') {
+                    if (t.charAt(j) == 'p')
+                        plateau[larg * i + j] = new PacGomme(i, j);
                     else
-                        plateau[larg*i+j] = new SuperPacGomme();
+                        plateau[larg * i + j] = new SuperPacGomme(i, j);
                 } else {
-                    plateau[larg*i+j] = null;
+                    plateau[larg * i + j] = null;
                 }
 
             }
@@ -42,15 +43,16 @@ public class Plateau{
             parsedLst[i] = Integer.parseInt(lst[i]);
         }
 
-        plateau[parsedLst[2]*larg+parsedLst[3]] = new Pacman();
-        plateau[parsedLst[4]*larg+parsedLst[5]] = new Inky(parsedLst[2], parsedLst[3], 1);
-        plateau[parsedLst[6]*larg+parsedLst[7]] = new Pinky(parsedLst[6], parsedLst[7], 1);
-        plateau[parsedLst[8]*larg+parsedLst[9]] = new Clyde(parsedLst[8], parsedLst[9], 1);
-        plateau[parsedLst[10]*larg+parsedLst[11]] = new Blinky(parsedLst[10], parsedLst[11], 1);
+        plateau[parsedLst[2] * larg + parsedLst[3]] = new Pacman();
+        plateau[parsedLst[4] * larg + parsedLst[5]] = new Inky(parsedLst[2], parsedLst[3], 1);
+        plateau[parsedLst[6] * larg + parsedLst[7]] = new Pinky(parsedLst[6], parsedLst[7], 1);
+        plateau[parsedLst[8] * larg + parsedLst[9]] = new Clyde(parsedLst[8], parsedLst[9], 1);
+        plateau[parsedLst[10] * larg + parsedLst[11]] = new Blinky(parsedLst[10], parsedLst[11], 1);
 
         read.close();
     }
-    public Entity getIndex(int index){
+
+    public Entity getIndex(int index) {
         return plateau[index];
     }
 
@@ -58,21 +60,21 @@ public class Plateau{
         return plateau;
     }
 
-    public static void main(String[] args)throws Exception {
+    public static void main(String[] args) throws Exception {
         Plateau j = new Plateau("level1.txt");
-        for(int i=0; i<j.plateau.length; i++)
-            if (i%28==0){
+        for (int i = 0; i < j.plateau.length; i++)
+            if (i % 28 == 0) {
                 System.out.println();
                 System.out.print(j.getIndex(i));
             }
     }
 
-    public void removeIndex(int index){
+    public void removeIndex(int index) {
         plateau[index] = null;
     }
 
     public void remove(Entity ent) {
-        for(int i = 0; i < plateau.length; i++) {
+        for (int i = 0; i < plateau.length; i++) {
             if (plateau[i] == ent) {
                 plateau[i] = null;
                 return;
