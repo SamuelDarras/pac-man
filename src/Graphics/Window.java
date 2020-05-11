70b3332beb370e1f7368c0f5c5acf2d61555505f
SamuelDarras <slamicote@gmail.com>
2020-05-11 17:47:57 +0200

    Somatoline

--- src/Graphics/Window.java
+++ src/Graphics/Window.java
@@ -1,4 +1,22 @@
 package Graphics;
 
-public class Window {
+
+import javafx.application.Application;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Window extends Application {
+    @Override
+    public void start(Stage primaryStage) {
+        primaryStage.setTitle("Hello World");
+        Parent root = new Parent() {
+        };
+        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
