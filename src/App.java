a623ec4706de93507b5db0bfab208d1f8e9817a9
SamuelDarras <slamicote@gmail.com>
2020-05-12 17:36:20 +0200

    TM

--- src/App.java
+++
@@ -1,8 +0,0 @@
-import Game.Partie;
-
-public class App {
-    public static void main(String[] args) throws Exception {
-        Partie p = new Partie();
-        p.run();
-    }
-}
--- src/Game/README.md
+++
@@ -1,8 +0,0 @@
-# Game package
-
-package contenant les classes du jeu :
-* Plateau.java
-
-# A faire
-Classes :
-* Partie.java
