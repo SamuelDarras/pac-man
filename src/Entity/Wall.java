2d3bfa0b8485327ff120304e51de241b67a9b377
SamuelDarras <slamicote@gmail.com>
2020-05-12 16:54:18 +0200

    Quelques ajouts de rien

--- src/Entity/Wall.java
+++ src/Entity/Wall.java
@@ -1,15 +1,15 @@
 package Entity;
 
 import Utils.Position;
-import Utils.Sizes;
+import Utils.Constants;
 
 public class Wall extends Entity{
     private Position<Double> pos;
 
     public Wall(double x, double y) {
-        super(x, y, Sizes.WALL_SIZE, Sizes.WALL_SIZE);
+        super(x, y, Constants.WALL_SIZE, Constants.WALL_SIZE);
     }
     public Wall(Position<Double> p) {
-        super(p, Sizes.WALL_SIZE, Sizes.WALL_SIZE);
+        super(p, Constants.WALL_SIZE, Constants.WALL_SIZE);
     }
 }
