70ec6f503d1a2e1c8146cf7896bdcc9ce1f77491
SamuelDarras <slamicote@gmail.com>
2020-05-12 16:53:54 +0200

    Quelques ajouts de rien

--- src/Game/Plateau.java
+++ src/Game/Plateau.java
@@ -1,11 +1,15 @@
 package Game;
 
 import Entity.*;
+import Utils.Constants;
 
 import java.io.*;
 
 public class Plateau {
     int idxFruit = 0;
+    int larg;
+
+    Pacman pacman;
 
     Entity[] plateau;
 
@@ -26,23 +30,27 @@ public class Plateau {
 
         int[][] murs = new int[Integer.parseInt(lst[0])][Integer.parseInt(lst[1])];
         Items[][] items = new Items[Integer.parseInt(lst[0])][Integer.parseInt(lst[1])];
-        int larg = murs[0].length;
+        larg = murs[0].length;
 
-        plateau = new Entity[murs[0].length * larg];
-
-        for (int i = 0; i < murs[0].length; i++) {
+        plateau = new Entity[larg * larg];
+        double x;
+        double y;
+        for (int i = 0; i < larg; i++) {
             t = read.readLine();
             for (int j = 0; j < larg; j++) {
+                x = i * 1.0*Constants.SCENE_WIDTH/getLargeur();
+                y = j * 1.0*Constants.SCENE_WIDTH/getLargeur();
+
                 if (t.charAt(j) == '1') {
-                    plateau[larg * i + j] = new Wall(i, j);
+                    plateau[larg * i + j] = new Wall(x, y);
                 }
-                if (t.charAt(j) != '0') {
+                else if (t.charAt(j) != '0') {
                     if (t.charAt(j) == 'p')
-                        plateau[larg * i + j] = new PacGomme(i, j);
+                        plateau[larg * i + j] = new PacGomme(x, y);
                     else
-                        plateau[larg * i + j] = new SuperPacGomme(i, j);
+                        plateau[larg * i + j] = new SuperPacGomme(x, y);
                 } else {
-                    plateau[larg * i + j] = null;
+                    plateau[larg * i + j] = new Entity(x, y, 1, 1);
                 }
 
             }
@@ -54,10 +62,11 @@ public class Plateau {
         }
 
         plateau[parsedLst[2] * larg + parsedLst[3]] = new Pacman();
-        plateau[parsedLst[4] * larg + parsedLst[5]] = new Inky(parsedLst[2], parsedLst[3], 1);
-        plateau[parsedLst[6] * larg + parsedLst[7]] = new Pinky(parsedLst[6], parsedLst[7], 1);
-        plateau[parsedLst[8] * larg + parsedLst[9]] = new Clyde(parsedLst[8], parsedLst[9], 1);
-        plateau[parsedLst[10] * larg + parsedLst[11]] = new Blinky(parsedLst[10], parsedLst[11], 1);
+        pacman = (Pacman) plateau[parsedLst[2] * larg + parsedLst[3]];
+        plateau[parsedLst[4] * larg + parsedLst[5]] = new Inky(parsedLst[2]* 1.0*Constants.SCENE_WIDTH/getLargeur(), parsedLst[3]* 1.0*Constants.SCENE_WIDTH/getLargeur(), 1);
+        plateau[parsedLst[6] * larg + parsedLst[7]] = new Pinky(parsedLst[6]* 1.0*Constants.SCENE_WIDTH/getLargeur(), parsedLst[7]* 1.0*Constants.SCENE_WIDTH/getLargeur(), 1);
+        plateau[parsedLst[8] * larg + parsedLst[9]] = new Clyde(parsedLst[8]* 1.0*Constants.SCENE_WIDTH/getLargeur(), parsedLst[9]* 1.0*Constants.SCENE_WIDTH/getLargeur(), 1);
+        plateau[parsedLst[10] * larg + parsedLst[11]] = new Blinky(parsedLst[10]* 1.0*Constants.SCENE_WIDTH/getLargeur(), parsedLst[11]* 1.0*Constants.SCENE_WIDTH/getLargeur(), 1);
 
         idxFruit = parsedLst[12] * larg + parsedLst[13];
 
@@ -70,6 +79,14 @@ public class Plateau {
 
     public Entity[] getPlateau() {
         return plateau;
+    }
+
+    public int getLargeur() {
+        return larg;
+    }
+
+    public Pacman getPacman() {
+        return pacman;
     }
 
     public int getIdxFruit() {
