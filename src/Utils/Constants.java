80eda2fe7680a62e9f433c656a65b3be5267d948
SamuelDarras <slamicote@gmail.com>
2020-05-12 16:54:44 +0200

    Renomage de classe

---
+++ src/Utils/Constants.java
@@ -0,0 +1,13 @@
+package Utils;
+
+public class Constants {
+    public static int SCENE_WIDTH       = 700;
+    public static int SCENE_HEIGHT      = 700;
+
+    public static double WALL_SIZE      = 1.0*SCENE_WIDTH/28;
+    public static int ITEM_DEFAULT_SIZE = 1;
+    public static int FRUIT_SIZE        = 2;
+    public static double PERSONNAGE_SIZE= 1.0*SCENE_WIDTH/28 * .9;
+    public static double PACMAN_SPEED   = 1.0*SCENE_WIDTH/400;
+
+}
