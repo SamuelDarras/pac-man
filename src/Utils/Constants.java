a45d881b97f6921308495e15bcb8dd256f407ccd
SamuelDarras <slamicote@gmail.com>
2020-05-14 17:56:11 +0200

    Modif.

--- src/Utils/Constants.java
+++ src/Utils/Constants.java
@@ -4,19 +4,27 @@ public class Constants {
     public static int larg;
     public static int haut;
 
+    public static double WALL_HEIGHT, WALL_WIDTH;
+
+    public static int SCENE_HEIGHT      = 700;
+    public static int SCENE_WIDTH       = 600;
+
+    public static double PERSONNAGE_SIZE;
+
     public static void Init(int l, int h) {
         larg = l;
         haut = h;
+
+        WALL_HEIGHT     = (double) SCENE_HEIGHT / (double) haut;
+        WALL_WIDTH      = (double) SCENE_WIDTH  / (double) larg;
+
+        PERSONNAGE_SIZE = ((double) SCENE_WIDTH / (double) larg) * .7;
+
+        System.out.println(WALL_WIDTH + " ; " + WALL_HEIGHT + " ; " + PERSONNAGE_SIZE);
     }
 
-    public static int SCENE_WIDTH       = 600;
-    public static int SCENE_HEIGHT      = 700;
-
-    public static double WALL_HEIGHT    = 1;
-    public static double WALL_WIDTH     = 1;
-    public static int ITEM_DEFAULT_SIZE = 1;
+    public static int ITEM_DEFAULT_SIZE = (int) PERSONNAGE_SIZE/4;
     public static int FRUIT_SIZE        = 2;
-    public static double PERSONNAGE_SIZE= 1.0*SCENE_WIDTH/28 * .8;
     public static double PACMAN_SPEED   = 1.0*SCENE_WIDTH/400;
     public static double GHOST_SPEED    = PACMAN_SPEED * .9;
 
