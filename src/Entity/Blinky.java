3a937dc805e4a181d36514a18af935c21b6c1b13
SamuelDarras <slamicote@gmail.com>
2020-06-11 14:47:39 +0200

    la mort

--- src/Entity/Blinky.java
+++ src/Entity/Blinky.java
@@ -9,19 +9,10 @@ import javafx.scene.image.Image;
 import java.util.ArrayList;
 
 public class Blinky extends Ghost {
-  Image img = new Image("img/BlinkyGhost.png");
 
   public Blinky(double x, double y, double speed) {
     super(x, y, speed);
-  }
-
-  public void draw(GraphicsContext gc) {
-    super.draw(gc);
-
-    if (getDir() == Direction.LEFT)
-      gc.drawImage(img, getPos().getX() + getHitbox()[0], getPos().getY(), -getHitbox()[0], getHitbox()[1]);
-    else
-      gc.drawImage(img, getPos().getX(), getPos().getY(), getHitbox()[0], getHitbox()[1]);
+    img = new Image("img/BlinkyGhost.png");
   }
 
   public void tick(Pacman pac, Plateau p) {
