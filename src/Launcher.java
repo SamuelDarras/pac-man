167698a7c8c464bb93fafed70cecbcde2c6636bb
Samuel <slamicote@gmail.com>
2020-06-14 17:45:46 +0200

    laucher pour se lancer

---
+++ src/Launcher.java
@@ -0,0 +1,8 @@
+import Graphics.Window;
+import javafx.application.Application;
+
+public class Launcher {
+    public static void main(String[] args) {
+        Application.launch(Window.class, args);
+    }
+}
